e9aedb898860ba416653bcb347f97f0e2354a884
helgacozzo <helgacozzo@gmail.com>
2024-02-19 20:22:06 +0100

    Fix/Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/SideBar';
 import Home from './Pages/Home';
 import Events from './Pages/Events';
 import SingleEvent from './Pages/SingleEvent';
 import NotFound from './Pages/NotFound';
-import { useUser } from './context/UserContext'
-import Sidebar from './Components/SideBar';
 import SignUser from './Components/SignUser';
+import { useUser } from './context/UserContext';
 import "./App.scss";
 
-
 const App = () => {
-
     const { user } = useUser();
 
     return (
@@ -23,16 +20,14 @@ const App = () => {
 
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/signup" element={!user ? <SignUser type='signup' /> : <Navigate to="/" />} />
-                <Route path="/login" element={!user ? <SignUser type='login' /> : <Navigate to="/" />} />
                 <Route path="/events" element={<Events />} />
                 <Route path="/events/:id" element={<SingleEvent />} />
+                <Route path="/signup" element={!user ? <SignUser type='signup' /> : <Navigate to="/" />} />
+                <Route path="/login" element={!user ? <SignUser type='login' /> : <Navigate to="/" />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
-
         </>
     );
-
 };
 
 export default App;
